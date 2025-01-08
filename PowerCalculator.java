da3c301e520d714894095ca4254f231117ff0cb4
omnema007 <nemaom2003@gmail.com>
2025-01-08 11:09:51 +0530

    add PowerCalculator file

---
+++ PowerCalculator.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class PowerCalculator {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the base: ");
+        double base = sc.nextDouble();
+
+        System.out.println("Enter the exponent: ");
+        double exponent = sc.nextDouble();
+
+        double result = Math.pow(base, exponent);
+
+        System.out.println(base + " raised to the power of " + exponent + " is: " + result);
+
+        
+    }
+}
